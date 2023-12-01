34f53de68764134b2983f0e7a93c1f3858914b9a
Andrey Tynavi <andrejtynavi@MacBook-Air-Andrej.local>
2023-12-01 23:45:42 +1000

    Написали код по 2 задаче

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] a = new int[10];
+int count = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("a[{0}]=", i);
+    a[i] = Convert.ToInt32(Console.ReadLine());
+    if ((a[i] % 2) == 0)
+    count++;
+}
+Console.WriteLine(count);
